0cab1b8a685aec63095c52b378fcd2a1e4474b6d
ShamirLeon <leonshamir1@gmail.com>
2023-02-06 21:29:50 -0500

    SetUp - Theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,20 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
+		colors: {
+			transparent: 'transparent',
+			'Dark-Blue': '#022B3A',
+			'Accent-Blue': '#1F7A8C',
+			'Light-Accent-Blue': '#BFDBF7',
+			'Light-Gray': 'E1E5F2',
+			'Light-Green': '#74E7DA',
+			White: '#FFFFFF',
+			Black: '#000000',
+		},
+		fontFamily: {
+			Roboto: 'Roboto, sans-serif;',
+			Sora: 'Sora, sans-serif;',
+		},
 		extend: {},
 	},
 	plugins: [],
