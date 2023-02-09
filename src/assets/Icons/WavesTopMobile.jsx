a03a904217b4dc5913f7aa79e1a2563d9fd46cef
ShamirLeon <leonshamir1@gmail.com>
2023-02-08 21:48:17 -0500

    Add Waves Mobiles JSX

---
+++ src/assets/Icons/WavesTopMobile.jsx
@@ -0,0 +1,24 @@
+const WavesTopMobile = (props) => (
+    <svg
+      width={375}
+      height={100}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g clipPath="url(#a)">
+        <path
+          d="M275.03 52.967c-18.222 8.991-35.865 25.108-54.037 34.883-25.884 13.933-52.839 14.775-78.683.325C117.196 74.167 91.154 40 66.338 22.642 44.331 7.242 20.304.9-1 20.142V100h377V77.208c-32.191-32.378-67.11-40.762-100.97-24.241Z"
+          fill="#022B3A"
+        />
+      </g>
+      <defs>
+        <clipPath id="a">
+          <path fill="#fff" transform="rotate(-180 188 50)" d="M0 0h377v100H0z" />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+  
+  export default WavesTopMobile
+  
