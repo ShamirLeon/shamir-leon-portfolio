661f2ed612217aa2a03296662680a4fc9ddaa968
ShamirLeon <leonshamir1@gmail.com>
2023-02-18 12:03:20 -0500

    Add Top Border

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -6,6 +6,11 @@ const Skills = params => {
 			id='Skills'
 			className='relative z-30 mx-auto flex w-[300px] -translate-y-32 flex-col items-center gap-5 rounded-xl bg-White py-12 shadow-2xl'
 		>
+			<div className='w-full bg-[#E0E8F0] absolute top-0 rounded-t-xl flex px-6 py-2 gap-2'>
+				<div className='bg-[#3BB662] w-3 h-3 rounded-full'> </div>
+				<div className='bg-[#E5C30F] w-3 h-3 rounded-full'> </div>
+				<div className='bg-[#E75448] w-3 h-3 rounded-full'> </div>
+			</div>
 			<h2 className='text-3xl font-bold text-Accent-Blue'>Habilidades</h2>
 			<span className='h-[2px] w-24 bg-Light-Green'> </span>
 			{SkillsSection.Abilities.map(element => (
