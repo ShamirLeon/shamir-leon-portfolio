5a9024ac8fb86b20cb78f1b6d7875d316cc3fcee
ShamirLeon <leonshamir1@gmail.com>
2023-02-18 20:37:42 -0500

    Nav Bar - Desktop Design

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -2,7 +2,7 @@ import ShamirIcon from '../../assets/Images/Image-Nav.png';
 
 const NavBar = params => {
 	return (
-		<nav className='fixed z-50 flex w-full items-center justify-between gap-4 bg-White/80 py-[18px] px-6 font-Sora text-sm font-bold text-Dark-Blue backdrop-blur-sm'>
+		<nav className='fixed z-50 flex w-full items-center justify-between bg-White/80 py-[18px] px-6 font-Sora text-sm font-bold text-Dark-Blue backdrop-blur-sm lg:px-32'>
 			<a href='#Hero'>
 				<img
 					src={ShamirIcon}
@@ -10,7 +10,7 @@ const NavBar = params => {
 					className='min-w-10 w-10'
 				/>
 			</a>
-			<ul className='flex gap-4'>
+			<ul className='flex gap-4 lg:gap-12'>
 				<li>
 					<a href='#Skills'>Habilidades</a>
 				</li>
