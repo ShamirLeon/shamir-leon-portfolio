1e81e603628104dc7aeb388ece4fbc85987bb5b1
ShamirLeon <leonshamir1@gmail.com>
2023-02-20 20:16:19 -0500

    Projects - Desktop Design

--- src/components/ProjectsC/Projects.jsx
+++ src/components/ProjectsC/Projects.jsx
@@ -2,9 +2,9 @@ import ProjectList from './ProjectsList';
 
 const Projects = params => {
 	return (
-		<section className='px-6 pb-8' id='Projects'>
+		<section className='px-6 pb-8 lg:px-32' id='Projects'>
 			<div className=' flex flex-col gap-8'>
-				<div className='flex items-center justify-between'>
+				<div className='flex items-center justify-between '>
 					<h2 className='text-3xl font-bold'>Proyectos</h2>
 					<a
 						href='#Contact'
--- src/components/ProjectsC/ProjectsList.jsx
+++ src/components/ProjectsC/ProjectsList.jsx
@@ -10,28 +10,28 @@ const images = [TodoImg, BookmarkImg, BankImg];
 const ProjectsList = () => {
 	console.log(ProjectsSection[0].technologies.length);
 	return (
-		<div className='mx-auto flex flex-col gap-20'>
+		<div className='mx-auto flex flex-col gap-20 lg:mx-0 lg:flex-row lg:flex-wrap lg:justify-between'>
 			{ProjectsSection.map(card => (
 				<div
 					key={card.id}
-					className='max-w-[330px] rounded-xl py-[10px] px-3 shadow-xl'
+					className='max-w-[330px] rounded-xl py-[10px] px-3 shadow-xl lg:max-w-[300px]'
 				>
 					<picture>
 						<img
 							src={images[card.id]}
 							alt={card.alternative}
-							className='mb-4 rounded-xl'
+							className='mb-4 rounded-xl lg:mx-auto'
 						/>
 					</picture>
-					<h2 className='text-center font-Roboto text-xl font-bold uppercase italic text-Black'>
+					<h2 className='text-center font-Roboto text-xl font-bold uppercase italic text-Black lg:text-left lg:text-lg '>
 						{card.title}
 					</h2>
-					<div className='flex flex-col justify-center gap-5 overflow-hidden py-5'>
-						<div className='flex justify-center gap-3'>
+					<div className='flex flex-col justify-center gap-5 overflow-hidden py-5 lg:py-2'>
+						<div className='flex justify-center gap-3 lg:flex-wrap'>
 							{card.technologies.map((e, index) => (
 								<span
 									key={index}
-									className='rounded-2xl bg-Light-Accent-Blue py-1 px-4 text-sm italic text-Dark-Blue'
+									className='rounded-2xl bg-Light-Accent-Blue py-1 px-4 text-sm italic text-Dark-Blue lg:text-xs'
 								>
 									{e}
 								</span>
@@ -40,10 +40,10 @@ const ProjectsList = () => {
 
 						<Dots className='relative z-[11] mx-auto w-full scale-125' />
 
-						<div className='flex justify-center gap-3'>
+						<div className='flex justify-center gap-3 lg:justify-start'>
 							<a
 								href={card.repository}
-								className='after:content-[" "] relative font-Roboto text-base uppercase italic after:absolute after:bottom-0 after:right-0 after:left-0 after:mx-auto after:h-1 after:w-full after:bg-Light-Green'
+								className='after:content-[" "] relative font-Roboto text-base uppercase italic after:absolute after:bottom-0 after:right-0 after:left-0 after:mx-auto after:h-1 after:w-full after:bg-Light-Green lg:text-sm'
 								target='_blank'
 								rel='noreferrer'
 							>
@@ -51,7 +51,7 @@ const ProjectsList = () => {
 							</a>
 							<a
 								href={card.live}
-								className='after:content-[" "] relative font-Roboto text-base uppercase italic after:absolute after:-bottom-0 after:right-0 after:left-0 after:mx-auto after:h-1 after:w-full after:bg-Light-Green'
+								className='after:content-[" "] relative font-Roboto text-base uppercase italic after:absolute after:-bottom-0 after:right-0 after:left-0 after:mx-auto after:h-1 after:w-full after:bg-Light-Green lg:text-sm'
 								target='_blank'
 								rel='noreferrer'
 							>
