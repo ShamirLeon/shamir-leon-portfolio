67cae4ff81ddb2afecc496b9a24ab00fb3aacebf
ShamirLeon <leonshamir1@gmail.com>
2023-02-20 20:51:24 -0500

    fix Padding - Desktop Design

--- src/components/ProjectsC/Projects.jsx
+++ src/components/ProjectsC/Projects.jsx
@@ -2,7 +2,7 @@ import ProjectList from './ProjectsList';
 
 const Projects = params => {
 	return (
-		<section className='px-6 pb-8 lg:px-32' id='Projects'>
+		<section className='px-6 pb-8 lg:px-32 lg:py-16' id='Projects'>
 			<div className=' flex flex-col gap-8'>
 				<div className='flex items-center justify-between '>
 					<h2 className='text-3xl font-bold'>Proyectos</h2>
